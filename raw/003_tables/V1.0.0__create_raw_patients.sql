75b95775973435fbb2463d0934a548076438b057
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 12:15:25 +0530

    Update V1.0.0__create_raw_patients.sql

--- raw/003_tables/V1.0.0__create_raw_patients.sql
+++ raw/003_tables/V1.0.0__create_raw_patients.sql
@@ -1,1 +1,17 @@
-
+create or replace table raw_patients (
+    patient_id string,
+    patient_name string,
+    patient_age string,          
+    patient_gender string,
+    blood_group string,
+    contact_phone string,
+    email string,
+    address string,
+    city string,
+    state string,
+    pincode string,
+    emergency_contact string,
+    insurance_provider string,
+    govt_scheme string,
+    registration_date string     
+);
