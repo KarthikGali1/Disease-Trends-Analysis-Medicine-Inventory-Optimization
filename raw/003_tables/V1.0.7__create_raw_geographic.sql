fc0f50636721fd9b878d793b63a930b362232bb9
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 12:24:07 +0530

    Update V1.0.7__create_raw_geographic.sql

--- raw/003_tables/V1.0.7__create_raw_geographic.sql
+++ raw/003_tables/V1.0.7__create_raw_geographic.sql
@@ -1,1 +1,14 @@
-
+create or replace table raw_geographic (
+    region_id string,
+    state string,
+    city string,
+    tier_classification string,
+    district string,
+    pincode string,
+    latitude string,
+    longitude string,
+    state_population string,
+    population_density string,
+    healthcare_facilities_count string,
+    pharmacy_count string
+);
