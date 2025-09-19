93e79302627e16f9d65042ece8c5b8e6ee07454e
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 18:56:14 +0530

    Update V6.1.7__create_drugs_snowpipe.sql

--- raw/006_pipes/V6.1.7__create_drugs_snowpipe.sql
+++ raw/006_pipes/V6.1.7__create_drugs_snowpipe.sql
@@ -4,6 +4,6 @@ CREATE OR REPLACE PIPE raw_drugs_pipe
 AUTO_INGEST = TRUE
 AS
 COPY INTO RAW_DB_DEV.STAGING.RAW_DRUGS_WITH_IDS
-FROM @disease_inv_stage
+FROM @disease_inv_stage/drug_with_ids.csv
 FILE_FORMAT = (FORMAT_NAME = csv_ff)
 ON_ERROR = 'CONTINUE';
