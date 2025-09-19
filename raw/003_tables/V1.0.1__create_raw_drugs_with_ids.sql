bb6b813b41987c8a6d21c47a52d9d5e8f31cddc7
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 12:20:06 +0530

    Update V1.0.1__create_raw_drugs_with_ids.sql

--- raw/003_tables/V1.0.1__create_raw_drugs_with_ids.sql
+++ raw/003_tables/V1.0.1__create_raw_drugs_with_ids.sql
@@ -1,1 +1,18 @@
 
+create or replace table raw_drugs_with_ids (
+    drug_id string,
+    generic_name string,
+    therapeutic_class string,
+    dosage_form string,
+    strength string,
+    manufacturer string,
+    drug_controller_approval string,
+    shelf_life_months string,
+    storage_temperature string,
+    prescription_required string,
+    schedule string,
+    typical_daily_dose string,
+    min_duration_days string,
+    max_duration_days string,
+    max_single_prescription string
+);
