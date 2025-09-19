59186660fe472402e7e4b7a18465c8dfc94c9880
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 18:54:45 +0530

    Update V6.1.3__create_inventory_snowpipe.sql

--- raw/006_pipes/V6.1.3__create_inventory_snowpipe.sql
+++ raw/006_pipes/V6.1.3__create_inventory_snowpipe.sql
@@ -4,6 +4,6 @@ CREATE OR REPLACE PIPE raw_inventory_pipe
 AUTO_INGEST = TRUE
 AS
 COPY INTO RAW_DB_DEV.STAGING.RAW_INVENTORY
-FROM @disease_inv_stage
+FROM @disease_inv_stage/inventory.csv
 FILE_FORMAT = (FORMAT_NAME = csv_ff_inv)
 ON_ERROR = 'CONTINUE';
