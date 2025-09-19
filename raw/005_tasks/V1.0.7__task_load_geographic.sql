6245377f1a856c0612f1200f2d6764d1bedbd8d8
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 12:40:21 +0530

    Update V1.0.7__task_load_geographic.sql

--- raw/005_tasks/V1.0.7__task_load_geographic.sql
+++ raw/005_tasks/V1.0.7__task_load_geographic.sql
@@ -1,1 +1,17 @@
-
+CREATE OR REPLACE TASK task_load_geographic
+  WAREHOUSE = 'LOAD_WH'
+  SCHEDULE = 'USING CRON 40 2 * * * UTC'
+AS
+BEGIN
+  COPY INTO raw_geographic
+  FROM @disease_inv_stage/geographic_data.csv
+  FILE_FORMAT = csv_ff
+  ON_ERROR = 'continue';
+  
+  INSERT INTO load_audit_log 
+  SELECT 
+    'raw_geographic' as table_name,
+    COUNT(*) as record_count,
+    CURRENT_TIMESTAMP() as load_timestamp
+  FROM raw_geographic;
+END;
