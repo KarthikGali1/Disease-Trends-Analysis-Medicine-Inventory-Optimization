e36d504b3e8c2c3ad8fe4b65a3ab53f0414c9808
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 12:20:30 +0530

    Update V1.0.2__create_raw_suppliers.sql

--- raw/003_tables/V1.0.2__create_raw_suppliers.sql
+++ raw/003_tables/V1.0.2__create_raw_suppliers.sql
@@ -1,1 +1,13 @@
-
+create or replace table raw_suppliers (
+    supplier_id string,
+    supplier_name string,
+    supplier_city string,
+    supplier_state string,
+    supplier_gst_number string,
+    supplier_drug_license string,
+    contact_email string,
+    contact_phone string,
+    established_year string,
+    certification_status string,
+    supplier_type string
+);
