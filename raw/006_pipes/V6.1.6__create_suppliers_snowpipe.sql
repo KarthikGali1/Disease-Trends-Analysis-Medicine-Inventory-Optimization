3b8ac18809f70fa88aadcfb2c4d1f4665c074771
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 18:55:45 +0530

    Update V6.1.6__create_suppliers_snowpipe.sql

--- raw/006_pipes/V6.1.6__create_suppliers_snowpipe.sql
+++ raw/006_pipes/V6.1.6__create_suppliers_snowpipe.sql
@@ -4,6 +4,6 @@ CREATE OR REPLACE PIPE raw_supp_pipe
 AUTO_INGEST = TRUE
 AS
 COPY INTO RAW_DB_DEV.STAGING.RAW_SUPPLIERS
-FROM @disease_inv_stage
+FROM @disease_inv_stage/suppliers.csv
 FILE_FORMAT = (FORMAT_NAME = csv_ff)
 ON_ERROR = 'CONTINUE';
