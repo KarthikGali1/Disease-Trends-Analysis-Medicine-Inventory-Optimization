f2358e0e5a550a7913e5e120e52a35d6b381101e
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 12:38:57 +0530

    Update V1.0.3__task_load_hospitals.sql

--- raw/005_tasks/V1.0.3__task_load_hospitals.sql
+++ raw/005_tasks/V1.0.3__task_load_hospitals.sql
@@ -1,1 +1,17 @@
-
+CREATE OR REPLACE TASK task_load_hospitals
+  WAREHOUSE = 'LOAD_WH'
+  SCHEDULE = 'USING CRON 15 2 * * * UTC'
+AS
+BEGIN
+  COPY INTO raw_hospitals
+  FROM @disease_inv_stage/hospitals_data.csv
+  FILE_FORMAT = (FORMAT_NAME = csv_ff)
+  ON_ERROR = 'continue';
+  
+  INSERT INTO load_audit_log 
+  SELECT 
+    'raw_hospitals' as table_name,
+    COUNT(*) as record_count,
+    CURRENT_TIMESTAMP() as load_timestamp
+  FROM raw_hospitals;
+END;
