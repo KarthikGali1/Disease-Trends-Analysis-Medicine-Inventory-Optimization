751fef2715bd6c37fc9d7ede9abdff6b677c887b
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 12:22:53 +0530

    Create V1.0.9__create_raw_disease_with_ids.sql

---
+++ raw/003_tables/V1.0.9__create_raw_disease_with_ids.sql
@@ -0,0 +1,9 @@
+create or replace table raw_diseases_with_ids (
+    disease_id string,
+    disease string,
+    disease_category string,
+    severity_levels string,
+    transmission_type string,
+    seasonal_pattern string,
+    example_drugs string
+);
