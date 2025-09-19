c4f45ad48705c05d467e0fc11df0fe25ea49961e
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 12:23:15 +0530

    Update V1.0.3__create_raw_hospitals.sql

--- raw/003_tables/V1.0.3__create_raw_hospitals.sql
+++ raw/003_tables/V1.0.3__create_raw_hospitals.sql
@@ -1,1 +1,18 @@
-
+create or replace table raw_hospitals (
+    hospital_id string,
+    hospital_name string,
+    hospital_location string,
+    city string,
+    state string,
+    region string,
+    tier string,
+    pincode string,
+    hospital_type string,
+    hospital_tier string,
+    bed_capacity string,
+    established_year string,
+    latitude string,
+    longitude string,
+    contact_phone string,
+    email string
+);
