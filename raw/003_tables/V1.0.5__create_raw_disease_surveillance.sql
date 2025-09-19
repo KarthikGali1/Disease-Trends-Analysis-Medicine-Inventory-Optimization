6bdbbcf6a06069bc48b2b445c88c8d8d856b00aa
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 12:21:37 +0530

    Update V1.0.5__create_raw_disease_surveillance.sql

--- raw/003_tables/V1.0.5__create_raw_disease_surveillance.sql
+++ raw/003_tables/V1.0.5__create_raw_disease_surveillance.sql
@@ -1,1 +1,29 @@
+CREATE OR REPLACE TABLE raw_healthcare_surveillance (
+    surveillance_id STRING,
+    patient_id STRING,
+    patient_name STRING,
+    hospital_id STRING,
+    disease_id STRING,
+    region_id STRING,
+    date DATE,
+    admit_date DATE,
+    discharge_date DATE,
+    length_of_stay_days NUMBER,
+    patient_age NUMBER,
+    patient_gender STRING,
+    patient_symptoms STRING,
+    hospital_name STRING,
+    hospital_location STRING,
+    disease_name STRING,
+    state STRING,
+    city STRING,
+    case_count NUMBER,
+    severity_level STRING,
+    body_temperature FLOAT,
+    weather STRING,
+    monsoon_season STRING,
+    aqi NUMBER,
+    outcome STRING
+);
+
 
