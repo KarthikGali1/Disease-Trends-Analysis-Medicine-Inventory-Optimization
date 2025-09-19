4503bb1339c7c6b03925ef8b6c97acdc6f653566
karthik gali <gali.v.karthik@kipi.ai>
2025-09-19 12:26:37 +0530

    Create V1.0.2__create_csv_ff_inv.sql

---
+++ raw/001_file_formats/V1.0.2__create_csv_ff_inv.sql
@@ -0,0 +1,12 @@
+create or replace file format csv_ff_inv
+  type = 'CSV'
+  field_delimiter = ','
+  skip_header = 1
+  null_if = ('NULL', 'null', '########')  -- treat Excel overflow as NULL
+  empty_field_as_null = true
+  field_optionally_enclosed_by = '"'
+  trim_space = true
+  date_format = 'MM/DD/YYYY'              -- match your data
+  timestamp_format = 'MM/DD/YYYY HH24:MI:SS'
+  encoding = 'UTF8'
+  error_on_column_count_mismatch = false;
